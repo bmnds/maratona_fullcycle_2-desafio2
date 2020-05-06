ff9a32f32976285c2f07c9882bcb96d7f8c5fc9b
Bruno Mendes <bruno.mendes@prodam.am.gov.br>
2020-05-05 21:39:50 -0400

    Configurada dependência TypeOrm, desenvolvida entity Maratona e gerado script de Migration da versão inicial da base de dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [ TypeOrmModule.forRoot({
+    type: 'sqlite',
+    database: 'database.sqlite',
+    autoLoadEntities: true,
+  }) ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/maratona.entity.ts
@@ -0,0 +1,14 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Maratona {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  aula: string;
+
+  @Column()
+  url: string;
+  
+}
